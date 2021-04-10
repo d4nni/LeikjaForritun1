e724b08256383457f695c327c49b19d071149e43
Daníel Þór Ingólfsson <danieling19@tskoli.is>
2021-04-10 12:27:23 +0000

    Gera geymsluna

---
+++ CreateWithCode/Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{   
+    // breyta fyrir player, þannig hægt sé að tengja cameru við player
+    public GameObject player;
+    // offset breyta sem er föst, þannig að camera sé ekki inn í player, heldur aðeins fyrir aftan
+    private Vector3 offset = new Vector3(0, 6, -8);
+
+    // Update er kallað inn á hverjum ramma
+    void Update()
+    {
+        // færir cameru í samræmi við player og bætir við offsettinu til þess að hún haldist fyrir aftan
+        transform.position = player.transform.position + offset; 
+    }
+}
---
+++ CreateWithCode/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿// allar using innkallanir sem unity kallar inn sjálfkrafa
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// classinn sem stýrir player
+public class PlayerController : MonoBehaviour
+{
+    // breyta fyrir hraðann á ökutækinu
+    private float speed = 20.0f;
+    // breyta fyrir beygju hraðann
+    private float turnSpeed = 60.0f;
+    // breyta fyrir lárétt input
+    private float horizontalInput;
+    // breyta fyrir áfram eða lóðrétt input
+    private float forwardInput;
+
+    // Update er það fall sem kallað er inn á hverjum ramma
+    void Update()
+    {
+        // lárétt input tekur inn input á láréttan ás
+        horizontalInput = Input.GetAxis("Horizontal");
+        // áfram input tekur inn input á lóðréttum ás
+        forwardInput = Input.GetAxis("Vertical");
+
+        // færa ökutækið áfram, stjórnað með lóðréttu inputti
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // beygir ökutækinu til hægri eða vinstri, stjórnað með láréttu inputti
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
