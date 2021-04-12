fc894c36f2d4b07111c40301b04de0fdaedae750
Daníel Þór Ingólfsson <danieling19@tskoli.is>
2021-04-12 16:49:22 +0000

    baetti byssi

---
+++ Heimur1/Assets/Skriftur/ByssaSnu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ByssaSnu : MonoBehaviour
+{
+    public Transform kamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = kamera.rotation;
+    }
+}
